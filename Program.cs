a38885dc7abf4127418b2ad1a0c13905a60c1113
Greilyn Polanco <greilyntejada03@gmail.com>
2023-01-29 18:14:50 -0400

    Realizado Ejercicio 1 y 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Ejercicios{
+
+    public static void Main(string[] args){
+        Ejercicio();
+    }
+
+    public static void Ejercicio(){
+        //Ejercicio 1
+        System.Console.WriteLine("Greilyn Polanco Tejada");
+        //Ejercicio 5
+        System.Console.WriteLine("Me gusta la pasta mucho");
+    }
+}
+
